780428db0b6b95a1f246b45e2d933e498aca5f02
John Koerner <john.koerner@autodesk.com>
2016-06-15 18:55:04 -0400

    Initial Version

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosticsToolsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string leaky = "";
+
+            Console.WriteLine("Starting...");
+            HashSet<string> strings = new HashSet<string>();
+            HashSet<Worker> workers = new HashSet<Worker>();
+
+            for (int i=0; i<100000; i++)
+            {
+                leaky += i.ToString();
+                strings.Add(leaky);
+
+                Worker w = new Worker();
+                w.DoWork(i);
+
+                workers.Add(w);
+
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+
+    public class Worker
+    {
+        public void DoWork(int i)
+        {
+            try
+            {
+                if (i % 500 == 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(i));
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // Ignore it
+            }
+        }
+    }
+}
